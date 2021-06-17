38d8f4faa838ac794aee5c03d11311e93b1782fc
Iuri de Silvio <iurisilvio@gmail.com>
2021-06-17 11:32:54 -0300

    Remove Python 3.5.

--- setup.py
+++ setup.py
@@ -173,7 +173,6 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
@@ -186,7 +185,6 @@ setup(
     install_requires=[
         'django-appconf',
         'cryptography',
-        'setuptools-rust'
     ],
     tests_require=['Django'],
     cmdclass={
